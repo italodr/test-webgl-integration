6f751785d5ed40cd4fa59d8d27467367086e2b43
Italo Devoto Ramella <italodr@gmail.com>
2022-01-26 11:39:36 +0100

    Update script

--- gatsby-ssr.js
+++
@@ -1,5 +0,0 @@
-const React = require('react');
-
-exports.onRenderBody = ({ pathname, setPostBodyComponents }, pluginOptions) => {
-  setPostBodyComponents([<script key='app.webgl' type='module' src='/app.js' defer></script>]);
-};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 
 // styles
 const pageStyles = {
@@ -14,18 +15,36 @@ const section = {
 
 // markup
 const IndexPage = () => {
-  console.log('Index page created');
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/app.js';
+    script.type = 'module';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
       <section style={section}></section>
-      <section webgl='true' webgl-color-1='#ff4563' style={section}></section>
+      <section
+        webgl-dev='false'
+        webgl='true'
+        webgl-particles='false'
+        webgl-color-1='#5923d9'
+        webgl-color-2='#b223d9'
+        webgl-color-3='#d92366'
+        style={section}></section>
       <section style={section}></section>
       <section
+        webgl-dev='false'
         webgl='true'
-        webgl-color-1='#92fe8d'
-        webgl-color-2='#38cca3'
-        webgl-color-3='#87ff80'
+        webgl-particles='true'
+        webgl-color-1='#e2647d'
+        webgl-color-2='#f9ad65'
+        webgl-color-3='#74e1ac'
+        webgl-position='left'
+        webgl-image='https://www-backend-pre.kidsandus.com/sites/default/files/styles/original/public/2021-06/5-banner_1.png?itok=4NMZBYeQ'
         style={section}></section>
     </main>
   );
