7efee09fa1d14bdf4021e740cdf9c3837ea556ec
Italo Devoto Ramella <italodr@gmail.com>
2022-02-21 11:00:11 +0100

    Add sections for test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,39 @@
 import * as React from 'react';
 import { useEffect } from 'react';
-import Head from 'react-helmet';
-import { Link, navigate } from 'gatsby';
+import Helmet from 'react-helmet';
 
 import { getWebglScript } from '../ui/helpers';
 
-const pageStyles = {
-  color: '#232129',
-  padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif'
-};
-
-const section = {
+const Section = {
   height: '100vh',
   position: 'relative'
 };
+
+const Hero = {
+  height: 'calc(100vh - 3rem)',
+  position: 'relative'
+};
+
+const Neutral = {
+  background: '#2b2b2b',
+  color: '#fff',
+  height: '80vh',
+  position: 'relative',
+  alignItems: 'center',
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: '3rem',
+  fontWeight: '700'
+};
+
+const Header = {
+  background: '#f1f1f1',
+  alignItems: 'center',
+  display: 'flex',
+  height: '3rem',
+  gap: '1rem',
+  padding: '0 1rem'
+}
 
 const IndexPage = () => {
   useEffect(() => {
@@ -26,29 +45,32 @@ const IndexPage = () => {
     );
   }, []);
 
-  useEffect(getWebglScript, []);
+  useEffect(() => {
+    document.getElementById('webgl-script').addEventListener('load', () => {
+      getWebglScript();
+    });
+  }, []);
 
   return (
-    <main style={pageStyles}>
-      <Head>
+    <main>
+      <Helmet>
         <title>Home Page</title>
-        <script src='/app.js' type='module' async></script>
-      </Head>
-      <section>
-        <ul style={{ padding: '0 0 3rem' }}>
-          <li>
-            <Link to={'/sample'}>Link to page using Gatsby Link component</Link>
-          </li>
-          <li>
-            <button onClick={() => navigate('/sample')}>
-              Link to page using Gatsby navigate method
-            </button>
-          </li>
-          <li>
-            <a href={'/sample'}>Link to page using HTML a tag</a>
-          </li>
-        </ul>
-      </section>
+        <script src='/app.js' type='module' id='webgl-script'></script>
+        <style>
+          {`
+          html, body {
+            font-family: -apple-system, Roboto, sans-serif, serif;
+            margin: 0;
+          }
+        `}
+        </style>
+      </Helmet>
+      <header style={Header}>
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </header>
       <section
         data-webgl-particles='true'
         data-webgl-position='left'
@@ -59,7 +81,10 @@ const IndexPage = () => {
         data-webgl-color-2='#f9ad65'
         data-webgl-color-3='#74e1ac'
         data-webgl-image='https://www-pre-drupal.kidsandus.com/sites/default/files/styles/original_webp/public/2022-01/Billboard.png.webp?itok=y3GxuAb2'
-        style={section}></section>
+        style={Hero}></section>
+      <section style={Neutral}>
+        <span>Space section</span>
+      </section>
       <section
         data-webgl-dev='false'
         data-webgl='true'
@@ -70,15 +95,21 @@ const IndexPage = () => {
         data-webgl-color-3='#f8ae8e'
         data-webgl-position='right'
         data-webgl-image='https://www-backend-pre.kidsandus.com/sites/default/files/styles/original/public/2021-06/5-banner_1.png?itok=4NMZBYeQ'
-        style={section}></section>
-      {/* <section
+        style={Section}></section>
+      <section
         data-webgl-dev='false'
         data-webgl='true'
-        data-webgl-particles='false'
+        data-webgl-particles='true'
+        data-webgl-nurbs-mirror='true'
         data-webgl-color-1='#5923d9'
         data-webgl-color-2='#b223d9'
         data-webgl-color-3='#d92366'
-        style={section}></section> */}
+        data-webgl-position='left'
+        data-webgl-image='https://www-backend-pre.kidsandus.com/sites/default/files/styles/original/public/2021-06/5-banner_1.png?itok=4NMZBYeQ'
+        style={Section}></section>
+      <section style={Neutral}>
+        <span>Space section</span>
+      </section>
     </main>
   );
 };
